fddf5de27673a9df6a949b0e8c3afb072dd4ca93
dyep49 <dyep49@gmail.com>
2014-01-12 15:21:14 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaseballProject::Application.config.secret_token = '636ba0a5ff7e421847f7ef4af41b35b4dff4767e44e4f62e35f95c83930467cc0e3ba8ed2f1a159958949670992271ec1c0fb35f795b3825e3fb076659391557'
