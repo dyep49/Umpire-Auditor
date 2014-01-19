dff2c488737e4e90c513ca335b64ed65d4ca52af
dyep49 <dyep49@gmail.com>
2014-01-19 02:02:07 -0500

    column sort works

--- app/controllers/umpires_controller.rb
+++ app/controllers/umpires_controller.rb
@@ -1,9 +1,17 @@
 class UmpiresController < ApplicationController
 
 def index
-	# @umpire_array = Umpire.sort_by_performance(Umpire.all)
 	umpire_info = Umpire.read_umpire_csv
-	umpire_array = umpire_info.sort_by{|k,v| v[3]}.reverse
+	case params["sort"]
+	when "total_calls" 
+		umpire_array = umpire_info.sort_by{|k,v| v[2]}.reverse
+	when "total_incorrect_calls"
+		umpire_array = umpire_info.sort_by{|k,v| v[1]}.reverse
+	when "total_correct_calls"
+		umpire_array = umpire_info.sort_by{|k,v| v[0]}.reverse
+	else
+		umpire_array = umpire_info.sort_by{|k,v| v[3]}.reverse
+	end
 	@umpire_hash = {}
 	umpire_array.each do |umpire|
 		@umpire_hash[umpire[0]] = umpire[1]
